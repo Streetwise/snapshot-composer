fa42990b88566a43470ab8e890e4745366eff991
Oleg Lavrovsky <oleg@utou.ch>
2020-11-19 15:11:38 +0100

    Added click

--- main.py
+++ main.py
@@ -1,36 +1,36 @@
-##########
-##########
-import json, ast, yaml, os
-import geopandas as gpd
-import pandas as pd
-import numpy as np
+#!/usr/bin/env python3
+import os
+import yaml
+import click
 
-from jinja2 import Environment, FileSystemLoader
+@click.command()
+@click.argument('filename')
+def generator(filename):
+    """ Generates geospatial Data Packages """
+    from generator import generateDataPackage
+    from jinja2 import Environment, FileSystemLoader
 
-from generator import generateDataPackage
+    # Load input from user
+    config_data = yaml.load(open(filename), Loader=yaml.FullLoader)
+    env = Environment(loader = FileSystemLoader('.'), trim_blocks=True, lstrip_blocks=True)
 
-#####
-### Generation of .yaml file from template.txt and data_to_feed.yaml
-# data_to_feed.yaml, input from user
-# generated .yaml file is to be used for datapackage
+    # Load template
+    template = env.get_template('template/template.txt')
 
-# Load input from user
-config_data = yaml.load(open('template/data_to_feed.yml'), Loader=yaml.FullLoader)
-env = Environment(loader = FileSystemLoader('.'), trim_blocks=True, lstrip_blocks=True)
+    # Render input to template
+    output_from_parsed_template=(template.render(config_data))
 
-# Load template
-template = env.get_template('template/template.txt')
+    # Save generated .yaml file
+    for location in config_data['locations']:
+        name = location['name']
+        filename = location['geodata']
+        print("Processing location %s from %s" % (name, filename))
+        try:
+            os.mkdir('output/%s' % name)
+        except FileExistsError:
+            pass
+        generateDataPackage(output_from_parsed_template, location)
 
-# Render input to template
-output_from_parsed_template=(template.render(config_data))
 
-# Save generated .yaml file
-for location in config_data['locations']:
-    name = location['name']
-    filename = location['geodata']
-    print("Processing location %s from %s" % (name, filename))
-    try:
-        os.mkdir('output/%s' % name)
-    except FileExistsError:
-        pass
-    generateDataPackage(output_from_parsed_template, location)
+if __name__ == '__main__':
+    generator()
